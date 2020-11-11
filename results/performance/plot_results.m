97604885b7e1490e6847ccd95b1db99c1d8e0988
Lars Ivar Hatledal <laht@hials.no>
2020-11-11 22:20:02 +0100

    update performance stuff

---
+++ results/performance/plot_results.m
@@ -0,0 +1,25 @@
+clc,clear, close all;
+
+
+fmigo = readmatrix('fmigo.csv') ./ 1000;
+fmpy = readmatrix('fmpy.csv') ./ 1000;
+om = readmatrix('omsimulatorMat.csv') ./ 1000;
+omCsv = readmatrix('omsimulatorCsv.csv') ./ 1000;
+vico = readmatrix('vico.csv') ./ 1000;
+vicoCsv = readmatrix('vicoCsv.csv') ./ 1000;
+cosim = readmatrix('cosim.csv') ./ 1000;
+cosimCsv = readmatrix('cosimCsv.csv') ./ 1000;
+
+
+x = (1:5);
+y = [fmigo fmpy cosim cosimCsv om omCsv vico vicoCsv];
+
+figure;
+hold on;
+boxplot(y, 0);
+title('Performance benchmark');
+ylabel('Time[s]')
+set(gca,'xticklabel', {'fmigo';'fmpy';'cosim';'cosimCsv';'om';'omCsv';'vico';'vicoCsv'})
+grid on;
+
+print('figures/performance.eps', '-depsc')
--- testPerf.main.kts
+++ testPerf.main.kts
@@ -5,17 +5,14 @@ import kotlin.system.measureTimeMillis
 
 val currentDir = File(".")
 
-val measurments = mutableMapOf<String, MutableList<Long>>()
-
-val vicoMeasurments = mutableListOf<Long>()
-val fmigoMeasurments = mutableListOf<Long>()
-val omMeasurments = mutableListOf<Long>()
-val fmpyMeasurments = mutableListOf<Long>()
-val cosimMeasurments = mutableListOf<Long>()
+val measurements = mutableMapOf<String, MutableList<Long>>()
 
 File(currentDir, "results/fmigo").mkdirs()
 File(currentDir, "results/omsimulator").mkdirs()
 File(currentDir, "results/fmpy").mkdirs()
+val perfDir = File(currentDir, "results/performance").apply {
+    mkdirs()
+}
 
 val numRuns = 1
 val tStop = 1000
@@ -26,40 +23,76 @@ for (i in 1 .. numRuns) {
 
     println("Run $i of $numRuns")
 
+
     measureTimeMillis {
         "vico simulate-ssp -stop $tStop -dt $dt --no-parallel --no-log -p \"initialValues\" -res \"../results/vico\" QuarterTruck_${hz}hz.ssp".runCommand(
             currentDir
         )
     }.also { elapsed ->
         println("Invoking vico took ${elapsed}ms")
-        measurments.computeIfAbsent("vico") { mutableListOf() }.add(elapsed)
+        measurements.computeIfAbsent("vico") { mutableListOf() }.add(elapsed)
     }
 
-   /* measureTimeMillis {
+    measureTimeMillis {
+        "vico simulate-ssp -stop $tStop -dt $dt --no-parallel -p \"initialValues\" -res \"../results/vico\" QuarterTruck_${hz}hz.ssp".runCommand(
+            currentDir
+        )
+    }.also { elapsed ->
+        println("Invoking vicoCsv took ${elapsed}ms")
+        measurements.computeIfAbsent("vicoCsv") { mutableListOf() }.add(elapsed)
+    }
+
+
+
+    measureTimeMillis {
         "python ssp-launcher.py QuarterTruck_${hz}hz.ssp".runCommand(
             File(currentDir, "fmigo")
         )
     }.also { elapsed ->
         println("Invoking fmigo took ${elapsed}ms")
-        measurments.computeIfAbsent("fmigo") { mutableListOf() }.add(elapsed)
-    }*/
+        measurements.computeIfAbsent("fmigo") { mutableListOf() }.add(elapsed)
+    }
 
-   /* measureTimeMillis {
+
+
+    measureTimeMillis {
         "python QuarterTruck.py".runCommand(
             File(currentDir, "fmpy")
         )
     }.also { elapsed ->
         println("Invoking fmpy took ${elapsed}ms")
-        measurments.computeIfAbsent("fmpy") { mutableListOf() }.add(elapsed)
-    }*/
+        measurements.computeIfAbsent("fmpy") { mutableListOf() }.add(elapsed)
+    }
 
-   /* measureTimeMillis {
+
+
+    measureTimeMillis {
         "OMSimulator -t=$tStop --numProcs=1 ../QuarterTruck_${hz}hz.ssp".runCommand(
             File(currentDir, "omsimulator")
         )
     }.also { elapsed ->
-        println("Invoking omsimulator took ${elapsed}ms")
-        measurments.computeIfAbsent("omsimulator") { mutableListOf() }.add(elapsed)
+        println("Invoking omsimulatorMat took ${elapsed}ms")
+        measurements.computeIfAbsent("omsimulatorMat") { mutableListOf() }.add(elapsed)
+    }
+
+    measureTimeMillis {
+        "OMSimulator -t=$tStop --numProcs=1 ../QuarterTruck_${hz}hz_csv.ssp".runCommand(
+            File(currentDir, "omsimulator")
+        )
+    }.also { elapsed ->
+        println("Invoking omsimulatorCsv took ${elapsed}ms")
+        measurements.computeIfAbsent("omsimulatorCsv") { mutableListOf() }.add(elapsed)
+    }
+
+
+
+    measureTimeMillis {
+        "cosim run -d $tStop --output-config=\"LogConfig.xml\" --output-dir=\"../results/libcosim\" ../QuarterTruck_${hz}hz.ssp".runCommand(
+            File(currentDir, "libcosim")
+        )
+    }.also { elapsed ->
+        println("Invoking cosim took ${elapsed}ms")
+        measurements.computeIfAbsent("cosim") { mutableListOf() }.add(elapsed)
     }
 
     measureTimeMillis {
@@ -67,34 +100,16 @@ for (i in 1 .. numRuns) {
             File(currentDir, "libcosim")
         )
     }.also { elapsed ->
-        println("Invoking cosim took ${elapsed}ms")
-        measurments.computeIfAbsent("cosim") { mutableListOf() }.add(elapsed)
-    }*/
+        println("Invoking cosimCsv took ${elapsed}ms")
+        measurements.computeIfAbsent("cosimCsv") { mutableListOf() }.add(elapsed)
+    }
 
-
-    /*measureTimeMillis {
-        "vico simulate-ssp -stop 10 -dt 0.001 -log \"extra/LogConfig.xml\" -p \"initialValues\" -res \"../results/vico-proxy\" QuarterTruck_10_proxy.ssp".runCommand(
-            currentDir
-        )
-    }.also { elapsed ->
-        println("Invoking vico-proxy took ${elapsed}ms")
-        measurments.computeIfAbsent("vico-proxy") { mutableListOf() }.add(elapsed)
-    }*/
-
-    /*measureTimeMillis {
-        "cosim run -d 10 --output-config \"LogConfig.xml\" --output-dir=\"../results/libcosim\" ../QuarterTruck_10_proxy.ssp".runCommand(
-            File(currentDir, "libcosim")
-        )
-    }.also { elapsed ->
-        println("Invoking cosim-proxy took ${elapsed}ms")
-        measurments.computeIfAbsent("cosim-proxy") { mutableListOf() }.add(elapsed)
-    }*/
 
 }
 
-measurments.forEach { (t, u) ->
+measurements.forEach { (t, u) ->
 
-    val file = File("results", "$t.csv")
+    val file = File(perfDir, "$t.csv")
     FileWriter(file).buffered().use { fw ->
         fw.write(u.joinToString("\n"))
     }
