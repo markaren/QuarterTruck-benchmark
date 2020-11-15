bdf383a083071f930f42c977d0946c9b45436e3c
Lars Ivar Hatledal <laht@hials.no>
2020-11-15 23:07:24 +0100

    update

--- results/performance/plot_results.m
+++ results/performance/plot_results.m
@@ -1,26 +1,28 @@
 clc,clear, close all;
 
 
-fmigo = readmatrix('fmigo.csv') ./ 1000;
+%fmigo = readmatrix('fmigo.csv') ./ 1000;
 %fmpy = readmatrix('fmpy.csv') ./ 1000;
-om = readmatrix('omsimulator.csv') ./ 1000;
-omMat = readmatrix('omsimulatorMat.csv') ./ 1000;
-omCsv = readmatrix('omsimulatorCsv.csv') ./ 1000;
+%om = readmatrix('omsimulator.csv') ./ 1000;
+%omMat = readmatrix('omsimulatorMat.csv') ./ 1000;
+%omCsv = readmatrix('omsimulatorCsv.csv') ./ 1000;
 vico = readmatrix('vico.csv') ./ 1000;
 vicoCsv = readmatrix('vicoCsv.csv') ./ 1000;
+vicoSingle = readmatrix('vicoSingle.csv') ./ 1000;
+vicoSingleCsv = readmatrix('vicoSingleCsv.csv') ./ 1000;
 cosim = readmatrix('cosim.csv') ./ 1000;
 cosimCsv = readmatrix('cosimCsv.csv') ./ 1000;
 
 
-x = (1:5);
-y = [fmigo fmpy cosim cosimCsv om omMat omCsv vico vicoCsv];
+x = (1:6);
+y = [cosim cosimCsv vico vicoCsv vicoSingle];
 
 figure;
 hold on;
 boxplot(y, 0);
 title('Gunnerus performance benchmark');
 ylabel('Time[s]')
-set(gca,'xticklabel', {'fmigo';'fmpy';'cosim';'cosimCsv';'om';'omMat';'omCsv';'vico';'vicoCsv'})
+set(gca,'xticklabel', {'cosim';'cosimCsv';'vico';'vicoCsv';'vicoSingle'})
 grid on;
 %set(gca,'Ytick',0:10:170)
 
--- testPerf.main.kts
+++ testPerf.main.kts
@@ -14,18 +14,36 @@ val perfDir = File(currentDir, "results/performance").apply {
     mkdirs()
 }
 
-val numRuns = 1
-val tStop = 250
-val hz = 50
-val dt = 1.0/hz
+val numRuns = 10
+val tStop = 500
+val hz = 20
+val dt = 1.0 / hz
 
-for (i in 1 .. numRuns) {
+for (i in 1..numRuns) {
 
     println("Run $i of $numRuns")
 
 
-   /* measureTimeMillis {
-        "vico simulate-ssp -stop $tStop -dt $dt --no-parallel --no-log -p \"initialValues\" -res \"../results/vico\" gunnerus-trajectory-proxy.ssp".runCommand(
+    /*measureTimeMillis {
+        "vico simulate-ssp -stop $tStop -dt $dt --no-parallel --no-log -p \"initialValues\"  gunnerus-trajectory-proxy.ssp".runCommand(
+            currentDir
+        )
+    }.also { elapsed ->
+        println("Invoking vico took ${elapsed}ms")
+        measurements.computeIfAbsent("vicoSingle") { mutableListOf() }.add(elapsed)
+    }
+
+    measureTimeMillis {
+        "vico simulate-ssp -stop $tStop -dt $dt --no-parallel -p \"initialValues\" -res \"../results/vico\" gunnerus-trajectory-proxy.ssp".runCommand(
+            currentDir
+        )
+    }.also { elapsed ->
+        println("Invoking vico took ${elapsed}ms")
+        measurements.computeIfAbsent("vicoSingleCsv") { mutableListOf() }.add(elapsed)
+    }*/
+
+    /*measureTimeMillis {
+        "vico simulate-ssp -stop $tStop -dt $dt --no-log -p \"initialValues\" gunnerus-trajectory-proxy.ssp".runCommand(
             currentDir
         )
     }.also { elapsed ->
@@ -34,28 +52,27 @@ for (i in 1 .. numRuns) {
     }
 
     measureTimeMillis {
-        "vico simulate-ssp -stop $tStop -dt $dt --no-parallel -p \"initialValues\" -res \"../results/vico\" gunnerus-trajectory-proxy.ssp".runCommand(
+        "vico simulate-ssp -stop $tStop -dt $dt -p \"initialValues\" -res \"../results/vico\" gunnerus-trajectory-proxy.ssp".runCommand(
             currentDir
         )
     }.also { elapsed ->
         println("Invoking vicoCsv took ${elapsed}ms")
         measurements.computeIfAbsent("vicoCsv") { mutableListOf() }.add(elapsed)
-    }
+    }*/
 
 
-
-    measureTimeMillis {
+/*    measureTimeMillis {
         "python ssp-launcher.py gunnerus-trajectory-proxy.ssp".runCommand(
             File(currentDir, "fmigo")
         )
     }.also { elapsed ->
         println("Invoking fmigo took ${elapsed}ms")
         measurements.computeIfAbsent("fmigo") { mutableListOf() }.add(elapsed)
-    }
+    }*/
 
 
 
-    measureTimeMillis {
+    /* measureTimeMillis {
         "python QuarterTruck.py".runCommand(
             File(currentDir, "fmpy")
         )
@@ -64,14 +81,14 @@ for (i in 1 .. numRuns) {
         measurements.computeIfAbsent("fmpy") { mutableListOf() }.add(elapsed)
     }*/
 
-    measureTimeMillis {
+    /*measureTimeMillis {
         "OMSimulator -t=$tStop --numProcs=0 ../gunnerus-trajectory-proxy.ssp".runCommand(
             File(currentDir, "omsimulator")
         )
     }.also { elapsed ->
         println("Invoking omsimulator took ${elapsed}ms")
         measurements.computeIfAbsent("omsimulator") { mutableListOf() }.add(elapsed)
-    }
+    }*/
 
     /*measureTimeMillis {
         "OMSimulator -t=$tStop --numProcs=1 ../gunnerus-trajectory-proxy_mat.ssp".runCommand(
@@ -92,7 +109,6 @@ for (i in 1 .. numRuns) {
     }*/
 
 
-/*
     measureTimeMillis {
         "cosim run -d $tStop --output-config=\"LogConfig.xml\" --output-dir=\"../results/libcosim\" ../gunnerus-trajectory-proxy.ssp".runCommand(
             File(currentDir, "libcosim")
@@ -109,7 +125,7 @@ for (i in 1 .. numRuns) {
     }.also { elapsed ->
         println("Invoking cosimCsv took ${elapsed}ms")
         measurements.computeIfAbsent("cosimCsv") { mutableListOf() }.add(elapsed)
-    }*/
+    }
 
 
 }
